1ce46d4216b7ef8dfe4d7b7dc5591056aaa9c72e
Pawan Verma <pverma@dimagi.com>
2023-11-21 16:26:45 +0530

    Add refresh apps button, reload tables after refresh apps

--- commcare_connect/static/js/project.js
+++ commcare_connect/static/js/project.js
@@ -1,3 +1,8 @@
 import '../sass/project.scss';
 
 /* Project specific Javascript goes here. */
+
+document.body.addEventListener('appsRefreshed', function (evt) {
+  document.getElementById('refresh-learn-spinner').remove();
+  document.getElementById('refresh-deliver-spinner').remove();
+});
