65849e326b460f9632c4b3643e7abeb84ea0a2dd
Zandre Engelbrecht <zengelbrecht@dimagi.com>
2025-09-30 13:28:11 +0200

    filter by approved only

--- completed_work_analysis.py
+++ completed_work_analysis.py
@@ -3,7 +3,7 @@ from collections import defaultdict
 from django.db.models import Case, Count, F, IntegerField, Value, When
 from django.db.models.functions import Cast, Coalesce
 
-from commcare_connect.opportunity.models import CompletedWork
+from commcare_connect.opportunity.models import CompletedWork, CompletedWorkStatus
 
 
 def get_avg_completed_work():
@@ -11,18 +11,19 @@ def get_avg_completed_work():
     Get the average amount of completed work per user, categorized by delivery type and status.
     """
     aggregated_data = (
-        CompletedWork.objects.values(
+        CompletedWork.objects.filter(status=CompletedWorkStatus.approved)
+        .values(
             delivery_type_name=Coalesce(
                 "opportunity_access__opportunity__delivery_type__name", Value("no_delivery_type")
             ),
             status_name=F("status"),
         )
-        .annotate(user_count=Count("opportunity_access__user_id", distinct=True), total_completed_work=Count("id"))
+        .annotate(user_count=Count("opportunity_access__user_id", distinct=True), total_approved_work=Count("id"))
         .annotate(
             avg_completed_work_per_user=Case(
                 When(
                     user_count__gt=0,
-                    then=Cast(F("total_completed_work"), IntegerField()) / Cast(F("user_count"), IntegerField()),
+                    then=Cast(F("total_approved_work"), IntegerField()) / Cast(F("user_count"), IntegerField()),
                 ),
                 default=Value(0),
                 output_field=IntegerField(),
