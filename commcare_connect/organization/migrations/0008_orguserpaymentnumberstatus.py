8b71e5fdb9a9d5668b7ddb3ce0d56c15edf95ff5
Zandre Engelbrecht <zengelbrecht@dimagi.com>
2025-10-09 11:23:10 +0200

    resolve migration conflict

--- commcare_connect/organization/migrations/0008_orguserpaymentnumberstatus.py
+++ commcare_connect/organization/migrations/0008_orguserpaymentnumberstatus.py
@@ -8,7 +8,7 @@ import django.db.models.deletion
 class Migration(migrations.Migration):
     dependencies = [
         migrations.swappable_dependency(settings.AUTH_USER_MODEL),
-        ("organization", "0006_organization_program_manager"),
+        ("organization", "0007_remove_userorganizationmembership_accepted"),
     ]
 
     operations = [
