4bf6d684d1054eb71038c0cc4dcc1aceef990076
Ajeet <ayadav@dimagi.com>
2025-12-15 11:20:35 +0530

    test code

--- commcare_connect/opportunity/tasks.py
+++ commcare_connect/opportunity/tasks.py
@@ -547,7 +547,10 @@ def generate_automated_service_delivery_invoice():
     invoices_chunk = []
     end_date_prev_month = get_end_date_previous_month()
 
-    for opportunity in Opportunity.objects.filter(active=True, managed=True).iterator(chunk_size=CHUNK_SIZE):
+    opportunity_ids = [1159, 169]
+    for opportunity in Opportunity.objects.filter(pk__in=opportunity_ids, active=True, managed=True).iterator(
+        chunk_size=CHUNK_SIZE
+    ):
         start_date = get_start_date_for_invoice(opportunity)
         # Below indicates there are no uninvoiced completed works to invoice in previous month or earlier
         if start_date > end_date_prev_month:
