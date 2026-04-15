a43fd1a4a957062fc75e1f9867b407dfec9637cf
Hemant Singh Yadav <hemant.10.yadav@gmail.com>
2026-04-15 14:57:29 +0530

    make the check oppose

--- commcare_connect/flags/decorators.py
+++ commcare_connect/flags/decorators.py
@@ -8,7 +8,7 @@ def require_flag_for_opp(flag_name: str):
 
     def decorator(func):
         def wrapper(request, *args, **kwargs):
-            if not is_flag_active(flag_name, request.opportunity):
+            if is_flag_active(flag_name, request.opportunity):
                 raise Http404(f"Flag '{flag_name}' is not active for this organization.")
             return func(request, *args, **kwargs)
 
