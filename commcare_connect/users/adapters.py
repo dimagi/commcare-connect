a67b1a8739c6e06389d1b03ac67fe482bd698ed5
Sravan Reddy <sravfeyn@gmail.com>
2026-04-16 14:33:45 +0530

    Display error message when email already exists for social-login account

--- commcare_connect/users/adapters.py
+++ commcare_connect/users/adapters.py
@@ -1,9 +1,15 @@
 from typing import Any
 
 from allauth.account.adapter import DefaultAccountAdapter
+from allauth.account.utils import user_email
+from allauth.exceptions import ImmediateHttpResponse
 from allauth.socialaccount.adapter import DefaultSocialAccountAdapter
+from allauth.socialaccount.models import SocialLogin
+from allauth.utils import email_address_exists
 from django.conf import settings
+from django.contrib import messages
 from django.http import HttpRequest
+from django.shortcuts import redirect
 
 
 class AccountAdapter(DefaultAccountAdapter):
@@ -14,3 +20,16 @@ class AccountAdapter(DefaultAccountAdapter):
 class SocialAccountAdapter(DefaultSocialAccountAdapter):
     def is_open_for_signup(self, request: HttpRequest, sociallogin: Any):
         return getattr(settings, "ACCOUNT_ALLOW_REGISTRATION", True)
+
+    def pre_social_login(self, request: HttpRequest, sociallogin: SocialLogin):
+        if sociallogin.is_existing:
+            return
+        email = user_email(sociallogin.user)
+        if not email:
+            return
+        if email_address_exists(email):
+            messages.error(
+                request,
+                f"An account with the email {email} already exists. " "Please sign in with your email and password.",
+            )
+            raise ImmediateHttpResponse(redirect("account_login"))
