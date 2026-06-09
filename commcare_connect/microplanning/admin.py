50f16454f692aa6f1d473eda6b80fe11ded81918
Pawan Verma <pverma@dimagi.com>
2026-06-09 12:55:24 +0530

    Add user visit inline on admin

--- commcare_connect/microplanning/admin.py
+++ commcare_connect/microplanning/admin.py
@@ -1,12 +1,14 @@
 from django.contrib import admin
 
 from commcare_connect.microplanning.models import WorkArea
+from commcare_connect.opportunity.admin import UserVisitTabularInline
 
 
 @admin.register(WorkArea)
 class WorkAreaAdmin(admin.ModelAdmin):
     list_display = ["get_opp_name", "slug", "get_username"]
     search_fields = ["opportunity__name", "opportunity_access__user__username"]
+    inlines = [UserVisitTabularInline]
 
     @admin.display(description="Opportunity Name")
     def get_opp_name(self, obj):
