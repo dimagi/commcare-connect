51f457a2be2ea539a925fccb2ca7de17c8ebe7a8
Charl Smit <csmit@dimagi.com>
2026-07-08 16:53:39 +0200

    fixup! Add OCS task-type section endpoint and chatbot partials

--- commcare_connect/opportunity/tests/test_ocs_task_section.py
+++ commcare_connect/opportunity/tests/test_ocs_task_section.py
@@ -21,34 +21,34 @@ def test_context_disconnected_returns_next_url(rf, user):
     assert context["ocs_next_url"] == "/a/org/opportunity/task_types/"
 
 
-@pytest.mark.django_db
-def test_context_connected_lists_chatbots(rf, user):
-    SocialAccount.objects.create(user=user, provider="ocs", uid="uid-ocs")
-    request = rf.get("/a/org/opportunity/task_types/ocs_section/")
-    request.user = user
-
-    with patch(
-        "commcare_connect.opportunity.views.list_chatbots",
-        return_value=[("bot-1", "Bot One"), ("bot-2", "Bot Two")],
-    ):
-        context = get_ocs_task_section_context(request)
-
-    assert context["ocs_connected"] is True
+def _assert_lists_chatbots(context):
     assert context["chatbots"] == [("bot-1", "Bot One"), ("bot-2", "Bot Two")]
     assert "ocs_error" not in context
 
 
+def _assert_api_error(context):
+    assert context["ocs_error"] is True
+
+
 @pytest.mark.django_db
-def test_context_connected_api_error(rf, user):
+@pytest.mark.parametrize(
+    "list_chatbots_kwargs, assert_context",
+    [
+        ({"return_value": [("bot-1", "Bot One"), ("bot-2", "Bot Two")]}, _assert_lists_chatbots),
+        ({"side_effect": OcsApiError("boom")}, _assert_api_error),
+    ],
+    ids=["lists_chatbots", "api_error"],
+)
+def test_context_connected(rf, user, list_chatbots_kwargs, assert_context):
     SocialAccount.objects.create(user=user, provider="ocs", uid="uid-ocs")
     request = rf.get("/a/org/opportunity/task_types/ocs_section/")
     request.user = user
 
-    with patch("commcare_connect.opportunity.views.list_chatbots", side_effect=OcsApiError("boom")):
+    with patch("commcare_connect.opportunity.views.list_chatbots", **list_chatbots_kwargs):
         context = get_ocs_task_section_context(request)
 
     assert context["ocs_connected"] is True
-    assert context["ocs_error"] is True
+    assert_context(context)
 
 
 @pytest.mark.django_db
