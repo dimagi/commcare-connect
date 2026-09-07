4698f83a30a1744a4291c8dae1bb79dd27eb7968
Charl Smit <csmit@dimagi.com>
2026-09-07 10:46:37 +0200

    Show more fields in admin view org list

--- commcare_connect/organization/admin.py
+++ commcare_connect/organization/admin.py
@@ -77,7 +77,7 @@ class UserOrganizationMembershipInline(admin.TabularInline):
 @admin.register(Organization)
 class OrganizationAdmin(admin.ModelAdmin):
     form = OrganizationCreationForm
-    list_display = ["name", "created_by", "program_manager", "funder"]
+    list_display = ["name", "short_name", "slug", "created_by", "program_manager", "funder", "verified"]
     search_fields = ["name"]
     ordering = ["name"]
     inlines = [UserOrganizationMembershipInline]
