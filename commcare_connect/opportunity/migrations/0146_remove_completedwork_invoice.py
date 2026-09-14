f7ecd11f06fce2ff53abcce9828b29c23f95b5ea
Charl Smit <csmit@dimagi.com>
2026-09-14 13:40:13 +0200

    Comment out operations

--- commcare_connect/opportunity/migrations/0146_remove_completedwork_invoice.py
+++ commcare_connect/opportunity/migrations/0146_remove_completedwork_invoice.py
@@ -10,8 +10,8 @@ class Migration(migrations.Migration):
     ]
 
     operations = [
-        migrations.RemoveField(
-            model_name='completedwork',
-            name='invoice',
-        ),
+        # migrations.RemoveField(
+        #     model_name='completedwork',
+        #     name='invoice',
+        # ),
     ]
