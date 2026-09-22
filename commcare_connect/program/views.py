c52412ad338b13c647f1cfb676287e90a607cb11
Ajeet <ayadav@dimagi.com>
2026-09-22 11:40:46 +0530

    fix lint issue

--- commcare_connect/program/views.py
+++ commcare_connect/program/views.py
@@ -42,7 +42,6 @@ from commcare_connect.program.tasks import (
 
 from .utils import AccessLevel, program_access_level_from_request, programs_accessible_to_org
 
-
 ALLOWED_ORDERINGS = {
     "name": "name",
     "-name": "-name",
