8fd5bbec2011cd0ba245a58335bfff2cad15f7de
Hemant Singh Yadav <hemant.10.yadav@gmail.com>
2026-09-23 21:25:23 +0530

    add email notifying admins that a member joined

--- commcare_connect/organization/tasks.py
+++ commcare_connect/organization/tasks.py
@@ -1,8 +1,9 @@
 from allauth.utils import build_absolute_uri
 from django.template.loader import render_to_string
 from django.urls import reverse
+from django.utils.translation import gettext as _
 
-from commcare_connect.organization.models import OrganizationInvite
+from commcare_connect.organization.models import OrganizationInvite, UserOrganizationMembership
 from commcare_connect.utils.tasks import send_mail_async
 
 
@@ -26,4 +27,26 @@ def send_org_invite(invite_id):
         message=render_to_string("organization/email/invite.txt", context),
         recipient_list=[invite.email],
         html_message=render_to_string("organization/email/invite.html", context),
+    )
+
+
+def send_invite_accepted_notification(membership_id):
+    membership = UserOrganizationMembership.objects.select_related("organization", "user").get(pk=membership_id)
+    organization = membership.organization
+
+    admin_emails = [
+        email
+        for email in organization.get_member_emails(role=UserOrganizationMembership.Role.ADMIN)
+        if email != membership.user.email
+    ]
+    if not admin_emails:
+        return
+
+    member_name = membership.user.name or membership.user.email
+    context = {"membership": membership, "organization": organization, "member_name": member_name}
+    send_mail_async.delay(
+        subject=_("%(member)s joined '%(org)s' on Connect") % {"member": member_name, "org": organization.name},
+        message=render_to_string("organization/email/invite_accepted.txt", context),
+        recipient_list=admin_emails,
+        html_message=render_to_string("organization/email/invite_accepted.html", context),
     )
